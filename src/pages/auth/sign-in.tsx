866378d5f933ced3a720b921ebd2bdf6220b88ad
mgckaled <marcelgckaled@gmail.com>
2025-06-16 11:26:08 -0300

    feat: add login page with label and input components

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,14 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export function SignIn() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <>
       <title>Log-in</title>
-      <meta name="description" content="Sign in page of the application." />
+      <meta name="description" content="Log-in page of the application." />
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
 
-      {/* Main content */}
-      <div className="text-center">
-        <h1 className="text-2xl font-bold">Sign In</h1>
-        <p className="mt-4 text-gray-600">Please sign in to continue.</p>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
